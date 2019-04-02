32998a4ee06b785935064e6d86affd613e85d9ca
github username <github email address>
2019-04-02 17:22:49 -0400

    done

---
+++ challenge.js
@@ -0,0 +1,98 @@
+
+
+function init(){
+  //get counter element
+  const counter = document.getElementById("counter");
+  //get likes ul list class
+  const likes = document.querySelector("ul.likes");
+  //flag option to check for pause
+  var pause = false;
+
+  //set interval for incerementing counter every 1 second
+  setInterval(counterTimerCallback, 1000);
+
+  function counterTimerCallback(){
+    counter.innerHTML = parseInt(counter.innerHTML) + 1;
+  }
+
+  //minus button----------------------------------
+  const minusButton = document.getElementById("-");
+  minusButton.addEventListener('click', minusButtonClickHandler);
+
+  function minusButtonClickHandler(event){
+    if(pause){
+      return event;
+    }
+    else{
+      counter.innerHTML = parseInt(counter.innerHTML) - 1;
+    }
+  }
+
+  //add button----------------------------------
+  const addButton = document.getElementById("+");
+  addButton.addEventListener('click', addButtonClickHandler);
+
+  function addButtonClickHandler(event){
+    if(pause){
+      return event;
+    }
+    else{
+      counter.innerHTML = parseInt(counter.innerHTML) + 1;
+    }
+  }
+
+  //like button----------------------------------
+  const likeButton = document.getElementById("<3");
+  likeButton.addEventListener('click', likeButtonClickHandler);
+
+  function likeButtonClickHandler(event){
+    if(pause){
+      return event;
+    }
+    else{
+      let element = document.createElement('li');
+      element.innerHTML = `liked ${counter.innerHTML}`;
+      likes.appendChild(element);
+    }
+  }
+
+  //pause button----------------------------------
+  const pauseButton = document.getElementById("pause");
+  pauseButton.addEventListener('click', pauseButtonClickHandler);
+
+  function pauseButtonClickHandler(event){
+    pause = !pause;
+    if(pause){
+      pauseButton.innerHTML = "resume";
+    }
+    else {
+      pauseButton.innerHTML = "pause";
+    }
+  }
+
+  //comments-----------------------------------
+  const submitButton = document.getElementById('submit');
+  const inputField = document.getElementById("comment-form").querySelector("input");
+  const commentDisplay = document.getElementById('list');
+  let comments = document.createElement('p');
+  commentDisplay.appendChild(comments);
+
+
+  console.log(commentDisplay)
+  console.log(inputField.value)
+
+  submitButton.addEventListener('click', submitButtonClickHandler);
+
+  function submitButtonClickHandler(event){
+    event.preventDefault()
+    if(pause){
+      return event;
+    }
+    else {
+      comments.innerHTML = comments.innerHTML + "<br>" + inputField.value;
+      inputField.value = "";
+    }
+  }
+}
+
+init();
